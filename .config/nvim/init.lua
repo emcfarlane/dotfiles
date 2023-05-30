a8f980653251bac38cb06fde3fba492d5f447652
Edward McFarlane <emcfarlane000@gmail.com>
2023-05-30 16:45:04 +0100

    Vim luafied config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,6 @@
 -- based on: https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
 vim.g.copilot_filetypes = { VimspectorPrompt = false }
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -32,9 +31,13 @@ require("lazy").setup({
 		-- LSP Configuration & Plugins
 		'neovim/nvim-lspconfig',
 		dependencies = {
+			-- Automatically install LSPs to stdpath for neovim
+			{ 'williamboman/mason.nvim', config = true },
+			'williamboman/mason-lspconfig.nvim',
+
 			-- Useful status updates for LSP
 			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-			{ 'j-hui/fidget.nvim', opts = {} },
+			{ 'j-hui/fidget.nvim',       opts = {} },
 
 			-- Additional lua configuration, makes nvim stuff amazing!
 			'folke/neodev.nvim',
