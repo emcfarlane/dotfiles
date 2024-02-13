b5f049532d67eeecc66a6743b38445c3f2e97676
Edward McFarlane <emcfarlane000@gmail.com>
2024-02-12 23:30:44 -0500

    Nvim lualine theme auto

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -79,7 +79,7 @@ require("lazy").setup({
 		opts = {
 			options = {
 				icons_enabled = false,
-				theme = 'onedark',
+				theme = 'auto',
 				component_separators = '|',
 				section_separators = '',
 			},
